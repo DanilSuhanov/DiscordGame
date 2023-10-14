75b095cfbb02ef920342f08ef128d77cccf9c4a5
Данил <Muravey7511@mail.ru>
2023-10-14 14:09:32 +0300

    add findSpaceshipByTitle in repository

--- src/main/java/ru/suhanov/discordgame/repository/SpaceshipRepository.java
+++ src/main/java/ru/suhanov/discordgame/repository/SpaceshipRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.suhanov.discordgame.model.military.Spaceship;
 
+import java.util.Optional;
+
 @Repository
 public interface SpaceshipRepository extends JpaRepository<Spaceship, Long> {
     boolean existsSpaceshipByTitle(String title);
+    Optional<Spaceship> findSpaceshipByTitle(String title);
 }
